6c30fe3b448bef9a82b62aeb0b58b21e726c2778
aamp19 <51056791+aamp19@users.noreply.github.com>
2021-12-03 23:18:23 -0500

    Update LoginForm.jsx

--- market-ai/src/components/parts/LoginForm.jsx
+++ market-ai/src/components/parts/LoginForm.jsx
@@ -1,51 +1,73 @@
 import React from 'react'
 import Axios from "axios"
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import { useNavigate } from "react-router-dom";
+
+
 const LoginForm = () => {
     const [userName, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+    const history = useNavigate();
 
 
 
-    const login = () => {
+
+    const login = async (e) => {
+        // e.preventDefault()
         Axios.post('http://localhost:3001/api/login', {
             username: userName,
             password: password
-        }).then(() => {
-            alert('login worked')
+        }).then(async (response) => {
+            console.log(response.data.username)
+            if (response.data.username === undefined || response.data.password === undefined) {
+                console.log('if');
+                setError('Username and/or Password is incorrect')
+
+            } else {
+                console.log('else');
+                history("/search");
+            }
+
+
+
         })
+
     }
 
     //not working yet
-    const validate = () => {
-        Axios.get('http://localhost:3001/api/content').then((response) => {
-            console.log(response)
-        })
-    }
+
+
 
     return (
         <div>
             <div className="form">
                 <h3>Login To Your Account</h3>
-                <form action="search" >
-                    <p>
-                        <label htmlFor="user">Enter Username: </label>
-                    </p>
-                    <input type="text" id="user" onChange={(e) => {
-                        setUsername(e.target.value)
-                    }} />
+                {/* <form > */}
+                {error ? [error].map(e =>
 
-                    <p>
-                        <label htmlFor="pass">Enter Password: </label>
-                    </p>
-                    <input type="password" id="pass" onChange={(e) => {
-                        setPassword(e.target.value)
-                    }} />
+                    <p className="error-msg">{e}</p>
+                    // <p>hello world</p>
 
-                    <p>
-                        <button onClick={() => { login(); validate() }}>Login</button>
-                    </p>
-                </form>
+                ) : <div></div>}
+                <p>
+                    <label htmlFor="user">Enter Username: </label>
+                </p>
+                <input type="text" id="user" onChange={(e) => {
+                    setUsername(e.target.value)
+                }} />
+
+                <p>
+                    <label htmlFor="pass">Enter Password: </label>
+                </p>
+                <input type="password" id="pass" onChange={(e) => {
+                    setPassword(e.target.value)
+                }} />
+
+                <p>
+                    <button onClick={login}>Login</button>
+                </p>
+                {/* </form> */}
             </div>
         </div>
     )
