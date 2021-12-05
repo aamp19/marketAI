c357717e876ca64092b65b564bb6c09d8df3eb4e
aamp19 <51056791+aamp19@users.noreply.github.com>
2021-12-04 22:27:03 -0500

    Update SearcherTicker.jsx

--- market-ai/src/components/parts/SearcherTicker.jsx
+++ market-ai/src/components/parts/SearcherTicker.jsx
@@ -3,14 +3,24 @@ import Axios from "axios"
 
 const SearcherTicker = (props) => {
     const [tickerSymbol, setTickerSymbol] = useState('')
+    const [invalidtickerSymbol, setinvalidTickerSymbol] = useState('')
 
     const tickerAPI = () => {
         Axios.post('http://localhost:3001/api/ticker', {
             tickerSymbol: tickerSymbol
+            // invalidtickerSymbol: invalidtickerSymbol
         }).then(() => {
             alert('sent')
         })
     }
+
+    const errorticker = (e) => {
+        if (document.getElementById('tickerSymbol').value == 'appl') {
+            e.preventDefault()
+            setinvalidTickerSymbol('Invalid Ticker')
+        }
+    }
+
 
 
     return (
@@ -18,13 +28,19 @@ const SearcherTicker = (props) => {
 
             <h3 id="search-title">Get Predictions From Our Powerful ML Algorithm</h3>
             {/* <form action="" onSubmit={e => e.preventDefault()}> */}
-            <form action="dashboard">
+            <form onSubmit={errorticker} action="dashboard" id="customForm">
+                {invalidtickerSymbol ? [invalidtickerSymbol].map(e =>
+
+                    <p className="error-msg">{e}</p>
+                    // <p>hello world</p>
+
+                ) : <div></div>}
                 <input type="text" onChange={(e) => {
                     setTickerSymbol(e.target.value)
                 }} id="tickerSymbol" size="35" placeholder="Search for news, tickers, or companies" required />
                 <p>
 
-                    <button onClick={tickerAPI}>Search</button>
+                    <button id="submit" onClick={tickerAPI}>Search</button>
                 </p>
 
             </form>
